c614d2af85482208f1a38b1c0d3024891caa0dd8
John Hugg <jhugg@voltdb.com>
2017-01-03 10:33:06 -0500

    ENG-11633: Update copyright on source for 2017

--- build.gradle
+++ build.gradle
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (C) 2008-2016 VoltDB Inc.
+ * Copyright (C) 2008-2017 VoltDB Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
